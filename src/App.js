9f1cbdc065c499306c4f29ce91f44e650a1b9be1
marcin_ch <bukkosky@gmail.com>
2018-03-17 10:21:30 +0100

    get data

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
 import React, { Component } from 'react';
 import FetchForm from './components/FetchForm'
+import ListItem from './components/ListItem'
 import './App.css';
 
 const API_URL = 'https://sandbox-rkrajewski.firebaseio.com/photos.json?orderBy=%22id%22&startAt=0&endAt='
 
 class App extends Component {
 
-
+    state = {
+        fetchedData: []
+    };
 
     fetchDate = (numberOfItemsToFetch) => {
 
         fetch(API_URL + numberOfItemsToFetch)
             .then(response => response.json())
-                .then(json => {
-                    console.log(json)
+            .then(Object.values)
+                .then(fetchedData => {
+                    this.setState({
+                        fetchedData
+                    });
+                    console.log(fetchedData)
                 })
         };
 
 
   render() {
+    const {fetchedData} = this.state
+
     return (
       <div className="App">
-          <FetchForm onSubmit={this.fetchDate}/>
+          <FetchForm onSubmit={this.fetchDate} />
+
+         <div>
+             {fetchedData.map(({ id, title, image, rating}) => (
+                 <ListItem key={id}
+                           title={title}
+                           image={image}
+                           rating={rating}/>
+             ))}
+         </div>
       </div>
     );
   }
---
+++ src/components/ListItem.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class ListItem extends Component {
+
+
+    render() {
+        const {title, image, rating} =this.props
+        return (
+            <div>
+                <div>
+                    {image ?
+                        <img src={image} alt=""/> :
+                        <span> NO IMAGE :</span>
+                    }
+                </div>
+                <div>{title}</div>
+                <div>{rating}</div>
+            </div>
+        );
+    }
+
+}
+
+export default ListItem;
